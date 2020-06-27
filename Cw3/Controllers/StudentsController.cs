648f9b31bd95050fdebd654b4ea2156c981c8265
Maciej Adrych <MaciejA@bonair.com.pl>
2020-06-27 12:16:57 +0200

    Dodanie metod GetStudent 2 i 3

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -55,12 +55,17 @@ namespace Cw3.Controllers
             {
                 com.Connection = con;
                 com.CommandText = "select * from dbo.students where indexnumber='"+indexNumber+"'";
+                com.Parameters.AddWithValue("index", indexNumber);
+                
 
                 con.Open();
                 var dr = com.ExecuteReader();
                 if (dr.Read())
                 {
                     var st = new Student();
+
+
+
                     st.IndexNumber = dr["IndexNumber"].ToString();
                     st.FirstName = dr["FirsName"].ToString();
                     st.LastName = dr["LastName"].ToString();
@@ -69,10 +74,61 @@ namespace Cw3.Controllers
                 }
 
             }
-
-
-
                 return NotFound();
+        }
+        //Procedury
+        [HttpGet]
+        public IActionResult GetStudents2()
+        {
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "TestProc3";
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+
+                com.Parameters.AddWithValue("LastName", "Kowalski");
+
+                var dr = com.ExecuteReader();
+
+            }
+
+            return NotFound();
+        }
+        //transakcje
+        [HttpGet]
+        public IActionResult GetStudents3()
+        {
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "insert into Student(FirstName) values (@firstName)";
+
+                con.Open();
+                SqlTransaction transaction = con.BeginTransaction();
+
+
+                try
+                {
+
+
+                    int affectedRows = com.ExecuteNonQuery();
+
+                    com.CommandText = "update into ...";
+                    com.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                }
+                catch (Exception exc)
+                {
+                    transaction.Rollback();
+                }
+
+            }
+
+            return NotFound();
         }
 
         [HttpPost]
@@ -80,7 +136,7 @@ namespace Cw3.Controllers
         {
             
 
-            return Ok(student);
+            return Ok("Utworzona studenta");
         }
 
         [HttpPut("{id}")]
